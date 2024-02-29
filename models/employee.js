6d868d8386c81eeaa501d5ff9edb3ccb2702c24d
ostentaylor <osteenkm@gmail.com>
2024-02-29 10:59:56 +0300

    server changes

---
+++ models/employee.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Employeeschema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'Please add your first name']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please add your last name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add your email address'],
+        unique: true, // Ensure email addresses are unique
+        match: [ // Validate email format using a regex
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email address'
+        ]
+    },
+    employeeNumber: {
+        type: String, // Assuming you want employee number as a string
+        unique: true, // Ensure employee numbers are unique
+        required: true
+    },
+    salaryAmount: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Employee", Employeeschema);
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const connectToDB = require("./db")
+const mongoose = require('mongoose')
+const { config } = require('dotenv');
+config()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Helloo, Express.js Server!</h1>');
+});
+
+const port = process.env.port ||  3000;
+
+const startsProcess = async () => {
+
+    await connectToDB()
+    app.listen(port, ()=> {
+    console.log(`Server  is running at http://localhost:${port}`);
+    });
+    
+}
+startsProcess()
